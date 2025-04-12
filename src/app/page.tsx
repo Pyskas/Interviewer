677fc2cd0fb319ad07cee73983e2803d5c893391
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2025-04-12 14:47:59 +0500

    Создание проекта,подключение базы данных и сервиса для вебкамеры

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="m-10"> home page </div>
+  );
+}
